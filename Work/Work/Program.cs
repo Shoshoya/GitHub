abaad898b47a5ef9ee3e7357b5cfb33d0844faa5
Viktoriya Shoshoya <viktoriyashoshoya@MacBook-Pro-Viktoriya.local>
2023-01-31 18:07:22 +0100

    data

---
+++ Work/Work/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Work
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Назва магазину
+
+            // Колір магазину
+
+            // Поверхів в магазині
+
+            // Продукція
+
+            // Площа
+
+            Console.WriteLine("Назва магазину: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Колір магазину: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Поверхів в магазині: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Продукція: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Площа: ");
+            Console.ReadLine();
+        }
+    }
+}
