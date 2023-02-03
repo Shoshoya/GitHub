cd0694b209dde45ed4c7ac397b4b0fff8235a6f7
Viktoriya Shoshoya <viktoriyashoshoya@MacBook-Pro-Viktoriya.local>
2023-02-03 17:20:53 +0100

    data5

---
+++ LabWork/LabWork/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LabWork;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //навчальна система
+
+        Console.WriteLine("Введіть дані студента \n");
+        Console.WriteLine("Прізвище: ");
+        Console.ReadLine();
+
+        Console.WriteLine("|м'я: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Вік: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Номер телефону: ");
+        Console.ReadLine();
+
+        // дані стосовно навчального закладу
+
+        Console.WriteLine("Професія: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Факультет: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Курс: ");
+        Console.ReadLine();
+
+        Console.WriteLine("Корпоративна пошта: ");
+        Console.ReadLine();
+    }
+}
+
