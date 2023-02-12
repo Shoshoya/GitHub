b30a10c4870fd395bb369b473c762cd620e07922
Viktoriya Shoshoya <viktoriyashoshoya@MacBook-Pro-Viktoriya.local>
2023-02-12 16:26:42 +0100

    LabRobota

---
+++ LabRobota/LabRobota/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LabRobota;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //число х більше або дорівнює 5,
+        //та менше або дорівнює 15
+
+        //Введення вихідних даних
+        Console.Write("×=");
+        double x = double.Parse(Console.ReadLine());
+
+        //реалізація алгоритму
+        if (x >= 5 && x <= 15)
+            Console.WriteLine("true");
+        else
+            Console.WriteLine("false");
+        //
+        Console.ReadKey();
+    }
+}
+
